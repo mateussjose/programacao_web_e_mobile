12937618758abfd9306d79e79818cfc1577ddd6a
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2026-08-26 21:26:23 +0000

    concluir exercicio 1

---
+++ atividade-01/app/exercicio1/page.js
@@ -0,0 +1,28 @@
+export default function Exercicio1() {
+    return(
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                backgroundImage: 'url(/background.jpg)',
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                padding: "2rem"
+                
+            }}
+        >
+            <h1
+                style={{
+                    color: "white",
+                    fontWeight:  "bold",
+                    backgroundColor: "red",
+                    padding: "1rem",
+                    border: '2px solid green',
+                    display: 'inline-block',
+                }}
+            >
+                Primeiro exercício de CSS
+            </h1>
+        </div>
+    );
+}
