2d6fd9effb429d27948de5de7dc4cd802ae5b302
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2026-08-29 01:55:36 +0000

    concluir exercicio 3

---
+++ atividade-01/app/exercicio3/page.js
@@ -0,0 +1,46 @@
+export default function Exercicio3 () {
+    return(
+        <div
+            style={{
+            width: "40rem",
+            margin: "0 auto",
+            marginTop: "2rem"
+            }}
+        >
+            <p
+                style={{
+                fontFamily: "Arial, sans-serif",
+                textDecoration: "underline"
+                }}
+            >
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. 
+            </p>
+
+            <a 
+                href="https://portal.unicap.br/"
+                style={{
+                    fontWeight: "bold",
+                    fontFamily: "Times New Roman, serif",
+                    color: "lightblue"
+                }}
+            >
+            Site da UNICAP
+            </a>
+          
+            <br />
+            
+            <a
+                href="https://portal2.unicap.br/RM/web/app/edu/PortalEducacional/login/"
+                style={{
+                    fontWeight: "bold",
+                    fontFamily: "Times New Roman, serif",
+                    color: "lightblue"
+                }} 
+            >
+            Portal do Aluno da UNICAP
+            </a>
+            
+        </div>
+    );
+}
+
