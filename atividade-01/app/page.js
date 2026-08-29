5d21cb3d5475810baf06d951f8f43cf3dbd8f4d6
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2026-08-29 03:47:19 +0000

    atualizar home

--- atividade-01/app/page.js
+++ atividade-01/app/page.js
@@ -3,15 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Meu Site</h1>
+      <h1>Atividade 01</h1>
 
-      <p>Escolha uma página:</p>
+      <br />
+
+      <p>Escolha um Exercicio:</p>
 
       <nav>
-        <Link href="/exercicio1">Página 1</Link>
-        <Link href="/exercicio2">Página 2</Link>
-        <Link href="/exercicio3">Página 3</Link>
-        <Link href="/exercicio4">Página 4</Link>
+        <Link href="/exercicio1" style={{ color: "lightblue"}}>Exercicio 1 </Link>
+        <Link href="/exercicio2" style={{ color: "lightblue"}}>Exercicio 2 </Link>
+        <Link href="/exercicio3" style={{ color: "lightblue"}}>Exercicio 3 </Link>
+        <Link href="/exercicio4" style={{ color: "lightblue"}}>Exercicio 4 </Link>
       </nav>
     </main>
   );
